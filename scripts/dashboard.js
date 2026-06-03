1ef6f835f7859d5bd861ef12c7435988fba4d0a4
CodesbyAyushShivam <ayushshivam72452gmail.com>
2026-06-03 13:31:19 +0530

    Added left Navigation bar for screens bigger than 600px with responsive layout

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -9,3 +9,29 @@ function logout() {
     localStorage.removeItem("regNo");
     location.reload();
 }
+
+
+function openMarks() {
+    window.location.href = "marks.html";
+}
+
+function openNotices() {
+    window.location.href = "notices.html";
+}
+
+function openPayments() {
+    window.location.href = "payments.html";
+}
+
+window.addEventListener("DOMContentLoaded", async () => {
+    const regNo = localStorage.getItem("regNo");
+    const userData = await fetch("/data/login.json");
+    const users = await userData.json();
+    const student = users.students.find(u => u.regNo === regNo);
+    const firstName = student.name.split(" ")[0];
+
+    //dynamically add all the students data to the dashboard whereever required
+    document.querySelectorAll(".user-name").forEach(el => el.innerText = student.name);
+    document.querySelectorAll(".user-reg").forEach(el => el.innerText = `Reg: ${student.regNo}`);
+    document.querySelectorAll(".sem-info").forEach(el => el.innerHTML = `Sem ${student.semester} <strong>&middot;</strong> ${student.branch}`);
+});
